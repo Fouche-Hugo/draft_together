5140b22ddd93f84a4414bf1de05c5bfe09d29593
foucheh <hugo.fouche@reseau.eseo.fr>
2024-11-11 20:28:51 +0000

    feat: seo tags

--- front/app.vue
+++ front/app.vue
@@ -1,3 +1,12 @@
+<script setup lang="ts">
+useSeoMeta({
+  title: "Draft Together",
+  ogTitle: "Draft Together",
+  description: "Draft with your friends easily.",
+  ogDescription: "Draft with your friends easily.",
+});
+</script>
+
 <template>
   <main class="h-dvh bg-zinc-900">
     <NuxtPage />
